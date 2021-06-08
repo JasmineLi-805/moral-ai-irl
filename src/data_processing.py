3cd2ba52927d6347900c7e621b5c1f8005507e57
Jasmine Li <jasmineli@MacBook-Air.local>
2021-06-07 17:49:49 -0700

    starting data processing

---
+++ src/data_processing.py
@@ -0,0 +1,7 @@
+"""
+TODO: Convert the initial json file to csv file 
+        - set a 1-on-1 dict that matchs feature names
+        - store the json trials in correct csv format, add "co-op count"
+TODO: Add the "co-op count" to the features list
+        - able to load co-op count when loading data
+"""
