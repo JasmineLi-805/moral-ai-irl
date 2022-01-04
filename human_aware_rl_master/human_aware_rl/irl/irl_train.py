6cb12db5e5dd476188cbe6591581357390099fd5
Jasmine Li <jl9985@uw.edu>
2022-01-04 15:40:51 -0800

    T20 .

--- human_aware_rl_master/human_aware_rl/irl/irl_train.py
+++ human_aware_rl_master/human_aware_rl/irl/irl_train.py
@@ -157,7 +157,7 @@ if __name__ == "__main__":
     # inputs, targets, seq_lens = load_data()
     # print(f'input = {inputs.shape}')
     # print(f'targets = {targets.shape}')
-    TRIAL = 10
+    TRIAL = 20
 
     cwd = os.getcwd()
     save_dir = f'{cwd}/result/T{TRIAL}'
