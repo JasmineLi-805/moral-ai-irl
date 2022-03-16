2eb7acb030553a26eddd04c5100dffbd0ca06ec1
Jasmine Li <jasminel.lyr@gmail.com>
2022-03-15 21:17:21 -0700

    .

--- human_aware_rl_master/human_aware_rl/irl/config_model.py
+++ human_aware_rl_master/human_aware_rl/irl/config_model.py
@@ -54,7 +54,7 @@ def get_train_config(reward_func):
     sgd_minibatch_size = 800 if not LOCAL_TESTING else 800
 
     # Rollout length
-    rollout_fragment_length = 100
+    rollout_fragment_length = 10
     
     # Whether all PPO agents should share the same policy network
     shared_policy = True
@@ -106,7 +106,7 @@ def get_train_config(reward_func):
     evaluation_interval = 100 if not LOCAL_TESTING else 1
 
     # How many timesteps should be in an evaluation episode
-    evaluation_ep_length = 100
+    evaluation_ep_length = 10
 
     # Number of games to simulation each evaluation
     evaluation_num_games = 20
@@ -156,7 +156,7 @@ def get_train_config(reward_func):
     }
 
     # Max episode length
-    horizon = 100
+    horizon = 10
 
     # Constant by which shaped rewards are multiplied by when calculating total reward
     reward_shaping_factor = 0.0
