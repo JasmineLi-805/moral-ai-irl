9f38c00474a63bc8f7f8070817d5980058cd24d7
Jasmine Li <jl9985@uw.edu>
2022-05-03 12:55:20 -0700

    T33: RL iteration=300

--- human_aware_rl_master/human_aware_rl/irl/config_model.py
+++ human_aware_rl_master/human_aware_rl/irl/config_model.py
@@ -60,7 +60,7 @@ def get_train_config(reward_func):
     shared_policy = True
 
     # Number of training iterations to run
-    num_training_iters = 100 if not LOCAL_TESTING else 1
+    num_training_iters = 300 if not LOCAL_TESTING else 1
 
     # Stepsize of SGD.
     lr = 5e-5
@@ -103,7 +103,7 @@ def get_train_config(reward_func):
     save_freq = -1  # do not store intermediate RL agent results
 
     # How many training iterations to run between each evaluation
-    evaluation_interval = 100 if not LOCAL_TESTING else 1
+    evaluation_interval = 300 if not LOCAL_TESTING else 1
 
     # How many timesteps should be in an evaluation episode
     evaluation_ep_length = 15
