1973a6de5193af6849b80816e852203268f50e0d
Jasmine Li <jasminel.lyr@gmail.com>
2022-05-21 14:26:07 -0700

    numpy array to tensor

--- human_aware_rl_master/human_aware_rl/irl/maxent_irl_train.py
+++ human_aware_rl_master/human_aware_rl/irl/maxent_irl_train.py
@@ -148,11 +148,12 @@ if __name__ == "__main__":
     irl_config = config['irl_params']
     
     expertFE = getMAIDummyFE(config, irl_config)    # only uses mdp_params and env_params in config
-    agentFE = getRLAgentFE(config, irl_config)
+    expertFE = torch.from_numpy(expertFE)
     for i in range(n_epochs):
         # train a policy and get feature expectation
         config["environment_params"]["custom_reward_func"] = reward_model.get_rewards
         agentFE = getRLAgentFE(config, irl_config)
+        agentFE = torch.from_numpy(agentFE)
 
         # compute the reward for the agent
         agent_reward = reward_model.forward(agentFE)
