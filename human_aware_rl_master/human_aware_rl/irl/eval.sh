49dcc8773e13043b4a4b110084bd9d7c59e59ea1
Jasmine Li <jasminel.lyr@gmail.com>
2023-06-15 22:42:01 -0700

    evaluation script

--- human_aware_rl_master/human_aware_rl/irl/eval.sh
+++ human_aware_rl_master/human_aware_rl/irl/eval.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-for i in 25 50 75
-do
-   PYTHONPATH=../../:../../../ CUDA_VISIBLE_DEVICES=3 python evaluate_maxent.py --trial 23 --epoch $i --type noncoop
-done
+
+PYTHONPATH=../../:../../../ CUDA_VISIBLE_DEVICES=0,1 python train_modified_kitchen.py --trial 2 --type ltn --epoch 20 --layout coop_experiment_3
+
+PYTHONPATH=../../:../../../ CUDA_VISIBLE_DEVICES=0,1 python train_modified_kitchen.py --trial 2 --type wte --epoch 30 --layout coop_experiment_3
+
+PYTHONPATH=../../:../../../ CUDA_VISIBLE_DEVICES=0,1 python train_modified_kitchen.py --trial 2 --type ltn --epoch 20 --layout coop_experiment_4
+
+PYTHONPATH=../../:../../../ CUDA_VISIBLE_DEVICES=0,1 python train_modified_kitchen.py --trial 2 --type wte --epoch 30 --layout coop_experiment_4
