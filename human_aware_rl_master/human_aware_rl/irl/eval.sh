7f5b95968b4748daa117114443c3b3ee244bb1e4
Jasmine Li <jasminel.lyr@gmail.com>
2023-06-19 21:59:03 -0700

    update training scripts

--- human_aware_rl_master/human_aware_rl/irl/eval.sh
+++ human_aware_rl_master/human_aware_rl/irl/eval.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-PYTHONPATH=../../:../../../ CUDA_VISIBLE_DEVICES=0,1 python train_modified_kitchen.py --trial 2 --type ltn --epoch 20 --layout coop_experiment_3
+PYTHONPATH=../../:../../../ CUDA_VISIBLE_DEVICES=0,1 python train_modified_kitchen.py --trial 2 --type ltn --epoch 20 --layout coop_experiment_5
 
-PYTHONPATH=../../:../../../ CUDA_VISIBLE_DEVICES=0,1 python train_modified_kitchen.py --trial 2 --type wte --epoch 30 --layout coop_experiment_3
+PYTHONPATH=../../:../../../ CUDA_VISIBLE_DEVICES=0,1 python train_modified_kitchen.py --trial 2 --type wte --epoch 30 --layout coop_experiment_5
 
-PYTHONPATH=../../:../../../ CUDA_VISIBLE_DEVICES=0,1 python train_modified_kitchen.py --trial 2 --type ltn --epoch 20 --layout coop_experiment_4
+PYTHONPATH=../../:../../../ CUDA_VISIBLE_DEVICES=0,1 python train_modified_kitchen.py --trial 2 --type ltn --epoch 20 --layout coop_experiment_6
 
-PYTHONPATH=../../:../../../ CUDA_VISIBLE_DEVICES=0,1 python train_modified_kitchen.py --trial 2 --type wte --epoch 30 --layout coop_experiment_4
+PYTHONPATH=../../:../../../ CUDA_VISIBLE_DEVICES=0,1 python train_modified_kitchen.py --trial 2 --type wte --epoch 30 --layout coop_experiment_6
+
+PYTHONPATH=../../:../../../ CUDA_VISIBLE_DEVICES=0,1 python train_modified_kitchen.py --trial 2 --type ltn --epoch 20 --layout coop_experiment_7
+
+PYTHONPATH=../../:../../../ CUDA_VISIBLE_DEVICES=0,1 python train_modified_kitchen.py --trial 2 --type wte --epoch 30 --layout coop_experiment_7
